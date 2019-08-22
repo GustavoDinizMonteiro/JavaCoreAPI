ac66af17f830011e6b453f2d01ec2cdf23e03987
Gustavo Moneiro <dinizmonteirogustavo@gmail.com>
2019-08-22 20:52:55 -0300

    Config maven build.

--- src/main/java/monteiro/gustavo/core/java/server/App.java
+++ src/main/java/monteiro/gustavo/core/java/server/App.java
@@ -1,7 +1,20 @@
 package monteiro.gustavo.core.java.server;
 
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+        var server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+        server.start();
 	}
 }
