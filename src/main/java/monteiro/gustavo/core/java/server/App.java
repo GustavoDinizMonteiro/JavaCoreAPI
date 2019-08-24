bcea70d57db124044a1a16f5d38ba3d073ed81b0
Gustavo Moneiro <dinizmonteirogustavo@gmail.com>
2019-08-23 23:55:41 -0300

    Add rest resource.

--- src/main/java/monteiro/gustavo/core/java/server/App.java
+++ src/main/java/monteiro/gustavo/core/java/server/App.java
@@ -1,20 +1,29 @@
 package monteiro.gustavo.core.java.server;
 
+import io.undertow.Handlers;
 import io.undertow.Undertow;
-import io.undertow.server.HttpHandler;
-import io.undertow.server.HttpServerExchange;
-import io.undertow.util.Headers;
+import io.undertow.util.Methods;
 
 public class App {
 	public static void main(String[] args) {
-        var server = Undertow.builder()
-                .addHttpListener(8080, "localhost")
-                .setHandler(new HttpHandler() {
-                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
-                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                        exchange.getResponseSender().send("Hello World");
-                    }
-                }).build();
-        server.start();
+		Undertow.builder().addHttpListener(8080, "localhost")
+				.setHandler(Handlers.path().addPrefixPath("/api", Handlers.routing()
+						.add(Methods.GET,"/item", exchange -> {
+							exchange.getResponseSender().send("Get all");
+						})
+						.add(Methods.POST,"/item", exchange -> {
+							exchange.getResponseSender().send("post");
+						})
+						.add(Methods.GET,"/item/{itemId}", exchange -> {
+							exchange.getResponseSender().send("Get one"
+									+exchange.getQueryParameters().get("itemId").getFirst());
+						})
+						.add(Methods.PUT,"/item/{itemId}", exchange -> {
+							exchange.getResponseSender().send("put");
+						})
+						.add(Methods.DELETE,"/item/{itemId}", exchange -> {
+							exchange.getResponseSender().send("Delete");
+						})
+				)).build().start();
 	}
 }
